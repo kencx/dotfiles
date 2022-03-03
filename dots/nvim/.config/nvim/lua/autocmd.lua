46bfee2b2cd44279492cc8cab9e71fb2e675f808
kencx <kennethcheo1@gmail.com>
2022-03-04 00:44:45 +0800

    minor change to autocmd.lua

--- dots/nvim/.config/nvim/lua/autocmd.lua
+++ dots/nvim/.config/nvim/lua/autocmd.lua
@@ -7,7 +7,7 @@ vim.cmd([[
 
     augroup highlight_yank
     autocmd!
-    au TextYankPost * silent! lua vim.highlight.on_yank{higroup='IncSearch', timeout=700}
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup='IncSearch', timeout=700}
     augroup END
 
     augroup remove_whitespace
