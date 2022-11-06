b57e724d06c68d71b89abb8ff03ba4f8f4a451f5
kencx <kennethcheo@pm.me>
2022-11-06 17:14:40 +0800

    replace vim autocmd with nvim_create_autocmd

--- dots/nvim/.config/nvim/lua/autocmd.lua
+++ dots/nvim/.config/nvim/lua/autocmd.lua
@@ -1,39 +1,46 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufWritePost", {
+	pattern = "plugins.lua",
+	command = "PackerCompile",
+})
+
+-- highlight on yank
+autocmd("TextYankPost", {
+	command = "silent! lua vim.highlight.on_yank{higroup='IncSearch', timeout=700}",
+})
+
+-- set minimap colorscheme
+autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_set_hl(0, "MiniMapNormal", { fg = "#363646", bg = "#272727" })
+	end,
+})
+
+autocmd("TermOpen", {
+	pattern = "*",
+	command = "setlocal nonumber norelativenumber",
+})
+
+autocmd("FileType", {
+	pattern = "gitcommit",
+	command = "setlocal wrap spell",
+})
+
+autocmd("FileType", {
+	pattern = "help",
+	command = "wincmd L",
+})
+
+autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = "*.nomad",
+	command = "set ft=hcl",
+})
+
 vim.cmd([[
-
-    augroup plugin_autocompile
-    autocmd!
-	autocmd BufWritePost plugins.lua PackerCompile
-    augroup END
-
-    augroup highlight_yank
-    autocmd!
-    autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup='IncSearch', timeout=700}
-    augroup END
-
     augroup remove_whitespace
     autocmd!
     autocmd BufWrite * mark ' | silent! %s/\s\+$// | norm ''
     augroup END
-
-	augroup _help
-	autocmd!
-	autocmd FileType help wincmd L
-	augroup END
-
-	augroup _markdown
-	autocmd!
-	autocmd FileType text,markdown,tex setlocal textwidth=80
-	autocmd FileType markdown setlocal wrap
-	augroup END
-
-	augroup _git
-	autocmd!
-	autocmd FileType gitcommit setlocal wrap
-	autocmd FileType gitcommit setlocal spell
-	augroup END
-
-	augroup _terminal
-	autocmd!
-	autocmd TermOpen * setlocal nonumber norelativenumber
-	augroup END
 ]])
