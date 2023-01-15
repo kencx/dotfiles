16157a4843060cd2d44260a03a1f4f8ec2eff4b6
kencx <kennethcheo@pm.me>
2023-01-15 11:55:57 +0800

    consolidate markdown ftplugin

--- dots/nvim/.config/nvim/after/ftplugin/markdown.lua
+++ dots/nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,5 @@
 vim.cmd([[
-    autocmd FileType markdown setlocal textwidth=80 spell
-    autocmd FileType markdown setlocal wrap
-
+    setlocal textwidth=80 spell wrap
     highlight htmlH1 guifg=#C34043 gui=bold
     highlight htmlH2 guifg=#DCA561 gui=bold
     highlight htmlH3 guifg=#76946A gui=bold
