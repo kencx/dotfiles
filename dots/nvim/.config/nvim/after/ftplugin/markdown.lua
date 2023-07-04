8705042fd2f6ec9e741019b7cbfcff80849ddd9a
kencx <kennethcheo@pm.me>
2023-07-04 12:56:18 +0800

    Change conceallevel in markdown

--- dots/nvim/.config/nvim/after/ftplugin/markdown.lua
+++ dots/nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,5 @@
 vim.cmd([[
-    setlocal textwidth=80 spell wrap
+    setlocal textwidth=80 spell wrap conceallevel=2
     highlight htmlH1 guifg=#C34043 gui=bold
     highlight htmlH2 guifg=#DCA561 gui=bold
     highlight htmlH3 guifg=#76946A gui=bold
