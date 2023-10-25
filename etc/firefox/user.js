31dbc04983e46b06f9386bdead9b2e34bc511395
kencx <kennethcheo@pm.me>
2023-10-25 13:45:52 +0800

    Cleanup

--- etc/firefox/user.js
+++ etc/firefox/user.js
@@ -89,6 +89,6 @@ user_pref("media.videocontrols.picture-in-picture.enabled", false);
 user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
 user_pref("full-screen-api.warning.timeout", 0);
 
-user_pref("browser.startup.page", 3);
-user_pref("browser.startup.homepage", "file:///home/kenc/.mozilla/startpage/index.html");
+// user_pref("browser.startup.page", 3);
+// user_pref("browser.startup.homepage", "file:///home/kenc/.mozilla/startpage/index.html");
 
