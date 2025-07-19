2a97d618882a8aa71f44ff00847a71d821c89688
kencx <kennethcheo@pm.me>
2025-07-19 09:37:54 +0800

    Install terraform-ls instead of using lspcontainers

--- dots/nvim/.config/nvim/lua/lsp/settings/terraformls.lua
+++ dots/nvim/.config/nvim/lua/lsp/settings/terraformls.lua
@@ -1,11 +1,3 @@
-local lspcontainers_ok, lspcontainers = pcall(require, "lspcontainers")
-if not lspcontainers_ok then
-	return {
-        filetypes = { "hcl", "tf", "terraform", "tfvars" },
-    }
-end
-
 return {
-	cmd = lspcontainers.command("terraformls"),
 	filetypes = { "hcl", "tf", "terraform", "tfvars" },
 }
