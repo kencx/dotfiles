de1d353f83b9eb3349a7628f3ef68c81fff0f805
kencx <kennethcheo@pm.me>
2025-11-22 13:36:57 +0800

    Update rsync-home

--- dots/bin/bin/rsync-home.sh
+++ dots/bin/bin/rsync-home.sh
@@ -7,14 +7,21 @@ MOUNT_POINT=$1
 
 mount -v $DISK "$MOUNT_POINT"
 
-# wget https://raw.githubusercontent.com/rubo77/rsync-homedir-excludes/master/rsync-homedir-excludes.txt -O /var/tmp/ignorelist
-
 SECONDS=0
 rsync -a --delete \
+    --exclude-from=/home/kenc/dotfiles/etc/rsync-homedir-excludes.txt \
     --exclude='syncthing/sync/*' \
     --exclude='.cache/*' \
-    --exclude='tmp/*' \
+    --exclude='.env/*' \
+    --exclude='.ghcup' \
+    --exclude='.cabal' \
+    --exclude='.pyenv' \
+    --exclude='.ansible' \
+    --exclude='tmp' \
+    --exclude='Downloads/*' \
+    --exclude='dev/compose-stacks/qbittorrent/torrents/*' \
     "/home/kenc" "$MOUNT_POINT"
+
 echo "rsync to $MOUNT_POINT completed in ${SECONDS}s"
 
 umount -v "$MOUNT_POINT"
